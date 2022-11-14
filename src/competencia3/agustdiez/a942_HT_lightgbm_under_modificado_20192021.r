4c30e9fdc3329750c99252bb9012e629cf0d0463
rodrieja <rodriguezalejandro.e@gmail.com>
2022-11-14 02:14:19 +0000

    full months

--- src/competencia3/agustdiez/a942_HT_lightgbm_under_modificado_20192021.r
+++ src/competencia3/agustdiez/a942_HT_lightgbm_under_modificado_20192021.r
@@ -35,7 +35,7 @@ options(error = function() {
 })
 #------------------------------------------------------------------------------
 
-ksemilla  <- 936659
+ksemilla  <- 539573
 
 kcrossvalidation_folds  <- 5  #En caso que se haga cross validation, se usa esta cantidad de folds
 
